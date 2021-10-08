f44ae7d95aaa5fa364ba5a7caebaa9cd10427ab9
minseokim0629 <han8121449@naver.com>
2021-10-09 01:03:09 +0900

    4강 이벤트핸들러

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,20 @@
     <div>
       <h4>{{ products[0] }}</h4>
       <p>50만원</p>
+      <button v-on:click="increase1()">허위매물신고</button>
+      <span>신고수 : {{ 신고수[0] }}</span>
     </div>
     <div>
       <h4>{{ products[1] }}</h4>
       <p>60만원</p>
+      <button @click="increase2()">허위매물신고</button>
+      <span>신고수 : {{ 신고수[1] }}</span>
     </div>
     <div>
       <h4>{{ products[2] }}</h4>
       <p>70만원</p>
+      <button @click="increase3()">허위매물신고</button>
+      <span>신고수 : {{ 신고수[2] }}</span>
     </div>
   </div>
 </template>
@@ -32,9 +38,21 @@ export default {
     //여기에 데이터 보관, 데이터는 object 자료로 저장
     // {자료이름 : 자료내용}
     return {
+      신고수: [0, 0, 0],
       메뉴들: ["Home", "Shop", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
     };
+  },
+  methods: {
+    increase1() {
+      this.$set(this.신고수, 0, this.신고수[0] + 1);
+    },
+    increase2() {
+      this.$set(this.신고수, 1, this.신고수[1] + 1);
+    },
+    increase3() {
+      this.$set(this.신고수, 2, this.신고수[2] + 1);
+    },
   },
   components: {},
 };
